867cfcc5b606631099187d2fb5a1e706a6a43774
Abdelrhman Talat <abdelrhman@arabhosters.com>
2015-10-04 20:19:59 +0200

    Upgrade ButterKnife to version 7.0.1

--- app/src/main/java/io/jpotts18/android_mvp/domain/ui/LoginActivity.java
+++ app/src/main/java/io/jpotts18/android_mvp/domain/ui/LoginActivity.java
@@ -1,30 +1,27 @@
 package io.jpotts18.android_mvp.domain.ui;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
+import android.support.v7.app.ActionBarActivity;
 import android.widget.EditText;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.InjectView;
 import io.jpotts18.android_mvp.R;
 
 
 public class LoginActivity extends ActionBarActivity {
 
-    @InjectView(R.id.login_email_edit_text)
+    @Bind(R.id.login_email_edit_text)
     EditText loginEditText;
 
-    @InjectView(R.id.login_password_edit_text)
+    @Bind(R.id.login_password_edit_text)
     EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ButterKnife.inject(this);
+        ButterKnife.bind(this);
     }
 
 }
